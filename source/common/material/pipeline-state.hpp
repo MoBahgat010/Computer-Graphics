2fda0849649ade877b92b04005448c5b186ab6d8
MoBahgat010 <mbahgat503@gmail.com>
2026-03-24 14:16:02 +0200

    feature/FInsihed Req 5 + Req 3 which was needed to complete Req 5

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,32 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+            if(faceCulling.enabled) {
+                glEnable(GL_CULL_FACE);
+                glCullFace(faceCulling.culledFace);
+                glFrontFace(faceCulling.frontFace);
+            } else {
+                glDisable(GL_CULL_FACE);
+            }
+
+            if(depthTesting.enabled) {
+                glEnable(GL_DEPTH_TEST);
+                glDepthFunc(depthTesting.function);
+            } else {
+                glDisable(GL_DEPTH_TEST);
+            }
+
+            if(blending.enabled) {
+                glEnable(GL_BLEND);
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+                glBlendEquation(blending.equation);
+            } else {
+                glDisable(GL_BLEND);
+            }
+            
+            glDepthMask(depthMask ? GL_TRUE : GL_FALSE);
+            glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
         }
 
         // Given a json object, this function deserializes a PipelineState structure
